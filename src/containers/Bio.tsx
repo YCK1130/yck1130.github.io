e539e714a10d598fe5fa3712ac697237b7281870
YCK1130 <ken1130yang@gmail.com>
2025-02-10 04:32:42 +0800

    chore: remove unuse

--- src/containers/Bio.tsx
+++
@@ -1,6 +0,0 @@
-import bioString from "../contents/bio.md?raw";
-import ParagraphContainer from "./ParagraphContainer";
-
-export default function Bio(props: { id?: string }) {
-    return <ParagraphContainer id={props.id} title="Bio" content={bioString} />;
-}
